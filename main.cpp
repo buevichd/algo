0aad18febec9dc2a53d8f4fe653c97da1eb9f0d3
Dmitry Buevich <dbuyevich@tacitknowledge.com>
2019-06-25 11:16:00 +0300

    Removed std:: from main file, added operator << for pair.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ using dd = double;
 using vi = vector<int>;
 
 template <class T, class V>
-std::istream& operator>>(std::istream& in, std::pair<T, V>& pair) {
+istream& operator>>(istream& in, pair<T, V>& pair) {
     return in >> pair.first >> pair.second;
 }
 
 template <class T>
-std::istream& operator>>(std::istream& in, std::vector<T>& elements) {
+istream& operator>>(istream& in, vector<T>& elements) {
     for (size_t i = 0; i < elements.size(); ++i) {
         in >> elements[i];
     }
@@ -25,11 +25,16 @@ bool Contains(const Container& container, const Element& element) {
 }
 
 template <class T>
-std::ostream& operator<<(std::ostream& out, const std::vector<T>& elements) {
+ostream& operator<<(ostream& out, const vector<T>& elements) {
     for (const T& element : elements) {
         out << element << ' ';
     }
     return out;
+}
+
+template <class T, class V>
+ostream& operator<<(ostream& out, const pair<T, V>& pair) {
+    out << pair.first << ' ' << pair.second;
 }
 
 #define FOR(i,n) for(size_t i=0, i##_len=(n); i<i##_len; ++i)
